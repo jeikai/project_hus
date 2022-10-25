c401b1624367d948a0115eec96e3d18b7818525b
jeikai <98737616+jeikai@users.noreply.github.com>
2022-10-25 21:35:49 +0700

    sua path

--- database/homework.php
+++ database/homework.php
@@ -3,7 +3,7 @@
     $path = explode('/', $_SERVER['REQUEST_URI']);
     $sql = "SELECT * FROM exercisedetails RIGHT JOIN exercise ON exercisedetails.exerciseId = exercise.ExerciseId 
     JOIN classes ON exercise.classId = classes.classId 
-    WHERE exercise.classId IN (SELECT classId FROM results WHERE classId = $path[3] AND studentId = $path[4]) GROUP BY ExerciseName";
+    WHERE exercise.classId IN (SELECT classId FROM results WHERE classId = $path[3] AND studentId = $path[4])";
     $statement = $connection->prepare($sql);
     $statement->execute();
     $result = $statement->fetchAll();
